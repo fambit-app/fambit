3ebc64df573fffb11da09b322a9e170694f55e73
Mitchell Hentges <mitchhentges@protonmail.com>
2016-08-29 20:39:51 -0700

    Set fields on webextension popup

---
+++ app/extension/index.js
@@ -0,0 +1,5 @@
+window.onload = function() {
+    document.getElementById('private-key').textContent = 'private-key';
+    document.getElementById('public-key').textContent = 'public-key';
+    document.getElementById('recv-address').textContent = 'recv-address';
+};
