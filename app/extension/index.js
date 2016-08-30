a345575f1bc03a8366d15f66d96f4894f8389e86
Mitchell Hentges <mitchhentges@protonmail.com>
2016-08-29 21:23:36 -0700

    Shows WIF and address in plugin popup

--- app/extension/index.js
+++ app/extension/index.js
@@ -1,10 +1,11 @@
 const bitcoin = require('../bitcoin/bitcoin');
 
-const bitcoinAddress = new bitcoin.BitcoinAddress();
-const bork = bitcoinAddress.createAddress();
-console.log(bork);
+const bitcoinAddress = new bitcoin.BitcoinAddress(
+    (key, value) => console.log(`SAVE [${key}:${value}]`),
+    (key) => console.log(`GET  [${key}]`)
+);
+const pair = bitcoinAddress.createAddress();
 window.onload = function () {
-    document.getElementById('private-key').textContent = 'private-key';
-    document.getElementById('public-key').textContent = 'public-key';
-    document.getElementById('recv-address').textContent = 'recv-address';
+    document.getElementById('wif').textContent = pair.toWIF();
+    document.getElementById('address').textContent = pair.getAddress();
 };
