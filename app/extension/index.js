b99b7716e2140aca7129a3535512f57f3dbf83b1
Mitchell Hentges <mitchhentges@protonmail.com>
2016-08-29 21:00:16 -0700

    Set up npm scripts for building

--- app/extension/index.js
+++ app/extension/index.js
@@ -1,4 +1,8 @@
+const bitcoin = require('../bitcoin');
+const address = bitcoin.BitcoinAddress.createAddress();
+
 window.onload = function() {
+    console.log(address);
     document.getElementById('private-key').textContent = 'private-key';
     document.getElementById('public-key').textContent = 'public-key';
     document.getElementById('recv-address').textContent = 'recv-address';
