a2c6d3ce5bc2640b1820c2cfaa82c04194e46e3f
roddicks <sheldon.roddick@hotmail.com>
2016-09-01 18:07:05 -0700

    Create request for address balance

---
+++ app/bitcoin/servers.js
@@ -0,0 +1,18 @@
+class BitcoinServerRequests {
+    getBalance(address) {
+        const response = this.makeRequest("https://blockchain.info/address/" + address + "?format=json");
+        if (response) {
+            const json = JSON.parse(response.body);
+            return json["final_balance"];
+        }
+    }
+
+    makeRequest(url) {
+        const request = new XMLHttpRequest();
+        request.open( "GET", url, false);
+        request.send();
+        return request.responseText;
+    }
+}
+
+module.exports = BitcoinServerRequests;
