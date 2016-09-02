aae7e26979823d1a20b664da59146a75fb35eb58
Mitchell Hentges <mitchhentges@protonmail.com>
2016-09-01 19:13:17 -0700

    Incremental build

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,25 @@
 const gulp = require('gulp');
 
+gulp.task('dev', ['build', 'watch']);
 gulp.task('build', ['js', 'resources']);
+
+gulp.task('watch', function() {
+    gulp.watch('app/**', ['js']);
+    gulp.watch('resources/**', ['resources']);
+});
 
 gulp.task('js', function() {
     const browserify = require('browserify');
+    const incremental = require('browserify-incremental');
     const source = require('vinyl-source-stream');
-    return browserify('./app/extension/index.js')
-        .bundle()
+    const opts = {
+        debug: true,
+        cacheFile: './gen/index.js.cache'
+    };
+
+    const bundle = browserify('./app/extension/index.js', Object.assign(opts, incremental.args));
+    incremental(bundle);
+    return bundle.bundle()
         .pipe(source('index.js'))
         .pipe(gulp.dest('gen'));
 });
