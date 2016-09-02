7cd4f561aa3653320594c98d90856e50a0235e4a
Mitchell Hentges <mitchhentges@protonmail.com>
2016-09-01 19:18:27 -0700

    Add gulp dist task

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,7 @@ const gulp = require('gulp');
 
 gulp.task('dev', ['build', 'watch']);
 gulp.task('build', ['js', 'resources']);
+gulp.task('dist', ['dist-js', 'dist-resources']);
 
 gulp.task('watch', function() {
     gulp.watch('app/**', ['js']);
@@ -25,5 +26,24 @@ gulp.task('js', function() {
 });
 
 gulp.task('resources', function() {
-    gulp.src(['manifest.json', 'resources/*']).pipe(gulp.dest('gen'));
+    return gulp.src(['manifest.json', 'resources/*']).pipe(gulp.dest('gen'));
+});
+
+gulp.task('clean', function() {
+    const del = require('del');
+    return del.sync(['gen', 'dist']);
+});
+
+gulp.task('dist-js', function() {
+    const browserify = require('browserify');
+    const source = require('vinyl-source-stream');
+
+    const bundle = browserify('./app/extension/index.js');
+    return bundle.bundle()
+        .pipe(source('index.js'))
+        .pipe(gulp.dest('dist'));
+});
+
+gulp.task('dist-resources', function() {
+    return gulp.src(['manifest.json', 'resources/*']).pipe(gulp.dest('dist'));
 });
