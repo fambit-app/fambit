204b6eeb60d738090dad588ea27bd5cbf4831d37
Mitchell Hentges <mitchhentges@protonmail.com>
2016-09-06 19:46:22 -0700

    Fix lint

--- app/extension/background.js
+++ app/extension/background.js
@@ -1,15 +1,15 @@
-chrome.runtime.onMessage.addListener(function(request) {
+chrome.runtime.onMessage.addListener((request) => {
     if (request.action === 'PAGE_LOAD') {
         // Handle recipient from last page
-        const recipient = localStorage.getItem("recipient");
+        const recipient = localStorage.getItem('recipient');
         if (recipient !== null) {
-            console.log('donating to: ' + recipient);
-            localStorage.removeItem("recipient");
+            console.log(`donating to: ${recipient}`);
+            localStorage.removeItem('recipient');
         }
 
         // Store recipient from current page. Don't donate until next page load, so user has time to revoke donation
         if (request.recipient !== undefined) {
-            localStorage.setItem("recipient", request.recipient);
+            localStorage.setItem('recipient', request.recipient);
         }
     }
 });
