9425d46b6cc986b537af5d883a53438b89fd1c47
Mitchell Hentges <mitchhentges@protonmail.com>
2016-11-07 20:03:59 -0800

    Fix lint

--- app/extension/funded-popup.js
+++ app/extension/funded-popup.js
@@ -1,5 +1,5 @@
 document.addEventListener('DOMContentLoaded', () => {
-    document.getElementById('funded-ok').addEventListener('click', function() {
+    document.getElementById('funded-ok').addEventListener('click', () => {
         console.log('sup');
         localStorage.setItem('onboard-status', 'DONE');
         window.location.href = 'main-popup.html';
--- app/extension/main-popup.js
+++ app/extension/main-popup.js
@@ -1,47 +1,47 @@
 document.addEventListener('DOMContentLoaded', () => {
-    var mockData = [{
+    const mockData = [{
         date: '2016-08-01',
         site: 'bustanut.xyz/index.html',
         donation: 0.011
-    },{
+    }, {
         date: '2016-07-28',
         site: 'fuzzlesoft.ca/phazeball2/A_0.3.2_2/index.html',
         donation: 0.012
-    },{
+    }, {
         date: '2016-07-28',
         site: 'fuzzlesoft.ca/phazeball2/index.html',
         donation: 0.012
-    },{
+    }, {
         date: '2016-07-28',
         site: 'fuzzlesoft.ca/projects.html',
         donation: 0.012
-    },{
+    }, {
         date: '2016-07-28',
         site: 'fuzzlesoft.ca/about.html',
         donation: 0.012
-    },{
+    }, {
         date: '2016-07-28',
         site: 'fuzzlesoft.ca/index.html',
         donation: 0.012
-    },{
+    }, {
         date: '2016-07-26',
         site: 'www.lukeatsea.com/dda2',
         donation: 0.013
     }];
 
-    var historyTable = document.querySelector('.history tbody');
-    mockData.forEach(function(donation) {
-        var row = document.createElement('tr');
-        var date = document.createElement('td');
+    const historyTable = document.querySelector('.history tbody');
+    mockData.forEach((donation) => {
+        const row = document.createElement('tr');
+        const date = document.createElement('td');
         date.appendChild(document.createTextNode(donation.date));
-        var site = document.createElement('td');
+        const site = document.createElement('td');
         site.title = donation.site;
         site.appendChild(document.createTextNode(donation.site.split('/')[0]));
-        var fund = document.createElement('td');
-        fund.appendChild(document.createTextNode(donation.donation + 'BC'));
+        const fund = document.createElement('td');
+        fund.appendChild(document.createTextNode('${donation.donation}BC'));
         row.appendChild(date);
         row.appendChild(site);
         row.appendChild(fund);
         historyTable.appendChild(row);
-    })
+    });
 });
