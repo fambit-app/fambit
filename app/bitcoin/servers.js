56dc544f0e6c9b8b94b7d98c4c6625819cad6a99
roddicks <sheldon.roddick@hotmail.com>
2016-11-07 19:27:18 -0800

    arrow callback style changes

--- app/bitcoin/servers.js
+++ app/bitcoin/servers.js
@@ -3,76 +3,64 @@ class BitcoinServerRequests {
     getBalance(address) {
         const promise = this._getRequest('GET', `https://www.blockchain.info/address/${address}?format=json`);
 
-        promise.then(
-            (val) => {
-                const json = JSON.parse(val.body);
-                return json.final_balance;
-            }
-        )
-        .catch(
-            () => -1
-        );
+        promise.then((val) => {
+            const json = JSON.parse(val.body);
+            return json.final_balance;
+        })
+        .catch(() => -1);
     }
 
     getTransactionList(address) {
         const promise = this._getRequest(`https://www.blockchain.info/unspent?active=${address}`);
 
-        promise.then(
-            (val) => {
-                const json = JSON.parse(val.body);
-                return json.unspent_outputs;
-            }
-        )
-        .catch(
-            () => []
-        );
+        promise.then((val) => {
+            const json = JSON.parse(val.body);
+            return json.unspent_outputs;
+        })
+        .catch(() => []);
     }
 
 
     submitTransaction(hash) {
-        this._postRequest('POST', 'https://www.blockchain.info/pushtx', `tx=${hash}`);
+        this._postRequest('https://www.blockchain.info/pushtx', `tx=${hash}`);
     }
 
     _getRequest(url) {
-        return new Promise(
-            (resolve, reject) => {
-                const request = new XMLHttpRequest();
-                request.open('GET', url, true);
+        return new Promise((resolve, reject) => {
+            const request = new XMLHttpRequest();
+            request.open('GET', url, true);
 
-                request.onreadystatechange = function () {
-                    if (request.readyState === 4 && request.status === 200) {
-                        resolve(request.responseText);
-                    } else if (request.readyState === 4 && request.status >= 400) {
-                        reject(request.responseText);
-                    }
-                };
+            request.onreadystatechange = function () {
+                if (request.readyState === 4 && request.status === 200) {
+                    resolve(request.responseText);
+                } else if (request.readyState === 4 && request.status >= 400) {
+                    reject(request.responseText);
+                }
+            };
 
-                request.timeout = 5000;
-                request.send();
-            }
-        );
+            request.timeout = 5000;
+            request.send();
+        });
     }
 
     _postRequest(url, params) {
-        return new Promise(
-            (resolve, reject) => {
-                const request = new XMLHttpRequest();
-                request.open('POST', url, true);
+        return new Promise((resolve, reject) => {
+            const request = new XMLHttpRequest();
+            request.open('POST', url, true);
 
-                request.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
+            request.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
 
-                request.onreadystatechange = function () {
-                    if (request.readyState === 4 && request.status === 200) {
-                        resolve(request.responseText);
-                    } else if (request.readyState === 4 && request.status >= 400) {
-                        reject(request.responseText);
-                    }
-                };
+            request.onreadystatechange = function () {
+                if (request.readyState === 4 && request.status === 200) {
+                    resolve(request.responseText);
+                } else if (request.readyState === 4 && request.status >= 400) {
+                    reject(request.responseText);
+                }
+            };
 
-                request.timeout = 5000;
-                request.send(params);
-            }
-        );
+            request.timeout = 5000;
+            request.send(params);
+        });
     }
 }
 
