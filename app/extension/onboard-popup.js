66b7c3bd10fa2fb2e9d79999435fd4ca2c63557e
Mitchell Hentges <mitchhentges@protonmail.com>
2016-11-07 18:05:27 -0800

    Fix DOMContentLoaded typo

--- app/extension/onboard-popup.js
+++ app/extension/onboard-popup.js
@@ -4,7 +4,7 @@ const BitcoinPersistence = require('../bitcoin/persistence');
 const bitcoinPersistence = new BitcoinPersistence();
 const address = new bitcoin.BitcoinAddress(bitcoinPersistence);
 
-document.addEventListener('domcontentloaded', () => {
+document.addEventListener('DOMContentLoaded', () => {
     const addressText = document.querySelector('.bitcoin-address');
     addressText.innerHTML = address.getPublicKey();
 });
