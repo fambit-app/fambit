adf2bfbfa111fbfd0b1d06db805f3d6c4f7e92e9
Mitchell Hentges <mitchhentges@protonmail.com>
2016-11-12 23:52:04 -0800

    Mixed up brackets

--- app/extension/onboard-popup.js
+++ app/extension/onboard-popup.js
@@ -2,5 +2,5 @@ const address = require('../bitcoin/address');
 
 document.addEventListener('DOMContentLoaded', () => {
     const addressText = document.querySelector('.bitcoin-address');
-    addressText.innerHTML = address.fromStorage(localStorage.getItem.bind(localStorage).publicKey);
+    addressText.innerHTML = address.fromStorage(localStorage.getItem.bind(localStorage)).publicKey;
 });
