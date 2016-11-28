b84cc8fe15d6bab97c51e81a841f1464c06ee0fc
Mitchell Hentges <mitchhentges@protonmail.com>
2016-11-27 16:38:43 -0800

    Can now ban domains, cancel donations

--- test/pending-donations-tests.js
+++ test/pending-donations-tests.js
@@ -11,12 +11,12 @@ test('should not initially have any pending donations', t => {
 test('should store multiple donations', t => {
     const mock = new MockStore();
     const pending = new PendingDonations(mock.save, mock.retrieve);
-    pending.queue('A', 1, new Date(0));
-    pending.queue('B', 2, new Date(1));
+    pending.queue('A', 'spyu.ca', 1, new Date(0));
+    pending.queue('B', 'fuzzlesoft.ca', 2, new Date(1));
 
     t.deepEqual(pending.list(), [
-        {address: 'A', amount: 1, date: new Date(0)},
-        {address: 'B', amount: 2, date: new Date(1)}
+        {address: 'A', domain: 'spyu.ca', amount: 1, date: new Date(0)},
+        {address: 'B', domain: 'fuzzlesoft.ca', amount: 2, date: new Date(1)}
     ]);
 });
 
@@ -24,13 +24,13 @@ test('should run donations through transaction on commit', t => {
     const mock = new MockStore();
     const pending = new PendingDonations(mock.save, mock.retrieve);
 
-    pending.queue('A', 1, new Date(0));
-    pending.queue('B', 2, new Date(1));
+    pending.queue('A', 'spyu.ca', 1, new Date(0));
+    pending.queue('B', 'fuzzlesoft.ca', 2, new Date(1));
     const result = pending.commit();
 
     t.deepEqual(result, [
-        {address: 'A', amount: 1, date: new Date(0)},
-        {address: 'B', amount: 2, date: new Date(1)}
+        {address: 'A', domain: 'spyu.ca', amount: 1, date: new Date(0)},
+        {address: 'B', domain: 'fuzzlesoft.ca', amount: 2, date: new Date(1)}
     ]);
 });
 
@@ -38,8 +38,8 @@ test('should clear pending donations after transaction', t => {
     const mock = new MockStore();
     const pending = new PendingDonations(mock.save, mock.retrieve);
 
-    pending.queue('A', 1, new Date(0));
-    pending.queue('B', 2, new Date(1));
+    pending.queue('A', 'spyu.ca', 1, new Date(0));
+    pending.queue('B', 'fuzzlesoft.ca', 2, new Date(1));
     pending.commit();
 
     t.deepEqual(pending.list(), []);
