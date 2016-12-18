69ec0f33c489a341d0ee9b338da6a421aadc1b4c
Mitchell Hentges <mitch9654@gmail.com>
2016-12-18 10:44:37 -0800

    Properly handle dust Group transaction outputs

--- app/bitcoin/bitcoin-transaction.js
+++ app/bitcoin/bitcoin-transaction.js
@@ -1,17 +1,34 @@
 const bitcoinJS = require('bitcoinjs-lib');
 
+// The number of satoshis to pay to the miners, based on how many "outputs"/donations occurred.
+// According to http://www.coindesk.com/new-service-finds-optimum-bitcoin-transaction-fee, it's chill to provide a fee
+// of 10 satoshis per byte.
+// http://bitcoin.stackexchange.com/a/3011 says that each output is 34 bytes.
+// Ignoring fixed bytes, as well as byte cost for inputs for simplicity, and because I'd rather err on the side of
+// paying less fees, since a long confirmation delay is OK, considering that fambit donations are _very_ async in
+// nature (one week delay already, so ¯\_(ツ)_/¯
+const MINING_FEE_RATIO = 340;
+
 module.exports = function bitcoinTransaction(privateKey, publicKey, donations, http) {
-    console.log(donations);
-    const totalSatoshi = donations.reduce((total, donation) => total + donation.amount, 0) * 100; // μBTC -> satoshi
+    const donationCount = Object.keys(donations).length;
+    const miningFee = donationCount * MINING_FEE_RATIO;
+    let totalDonatedSatoshi = 0;
+
     const transaction = new bitcoinJS.TransactionBuilder();
-    donations.forEach((donation) => transaction.addOutput(donation.address, donation.amount * 100));
+    for (const address in donations) {
+        if (donations.hasOwnProperty(address)) {
+            const amount = donations[amount] * 100; // μBTC -> satoshi
+            transaction.addOutput(address, amount);
+            totalDonatedSatoshi += amount;
+        }
+    }
 
     // Total amount of bitcoin from "input transactions" to be used to pay for all the microdonations.
     // This bitcoin comes from all the transactions from the user -> the fambit wallet
     let inputSatoshi = 0;
     http.getTransactionList(publicKey).then((unspentInputs) => {
         let localIndex = 0;
-        while (inputSatoshi < totalSatoshi) {
+        while (true) {
             if (localIndex >= unspentInputs.length) {
                 throw new Error('Not enough bitcoin to perform donation');
             }
@@ -19,6 +36,17 @@ module.exports = function bitcoinTransaction(privateKey, publicKey, donations, h
             const inputToUse = unspentInputs[localIndex++];
             transaction.addInput(inputToUse.tx_hash, inputToUse.tx_index);
             inputSatoshi += inputToUse.value;
+
+            if (inputSatoshi > totalDonatedSatoshi) {
+                // Put "dust" back in receiving address. Leftover bitcoin not put in an output is given as a fee
+                // to the miner
+
+                const dust = inputSatoshi - totalDonatedSatoshi;
+                if (dust > 0) {
+                    transaction.addOutput(publicKey, dust - miningFee);
+                }
+                break;
+            }
         }
 
         transaction.sign(0, bitcoinJS.ECPair.fromWIF(privateKey));
--- test/pending-donations-tests.js
+++ test/pending-donations-tests.js
@@ -20,18 +20,19 @@ test('should store multiple donations', t => {
     ]);
 });
 
-test('should run donations through transaction on commit', t => {
+test('should group donations to same address on commit', t => {
     const mock = new MockStore();
     const pending = new PendingDonations(mock.save, mock.retrieve);
 
     pending.queue('A', 'spyu.ca', 1, new Date(0));
-    pending.queue('B', 'fuzzlesoft.ca', 2, new Date(1));
+    pending.queue('B', 'blockchain.info', 2, new Date(1));
+    pending.queue('A', 'fuzzlesoft.ca', 5, new Date(2));
     const result = pending.commit();
 
-    t.deepEqual(result, [
-        {address: 'A', domain: 'spyu.ca', amount: 1, date: new Date(0)},
-        {address: 'B', domain: 'fuzzlesoft.ca', amount: 2, date: new Date(1)}
-    ]);
+    t.deepEqual(result, {
+        A: 6,
+        B: 2
+    });
 });
 
 test('should clear pending donations after transaction', t => {
